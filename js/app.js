4d8fd0bb7df9901872a7e16640ad31b74f4ae052
irfanhsajid <irfanhsajid@gmail.com>
2021-09-13 13:01:02 +0600

    size changed

--- js/app.js
+++ js/app.js
@@ -17,9 +17,9 @@ const showProducts = (products) => {
       <div>
     <img class="product-image" src=${image}></img>
       </div>
-      <h3>${product.title}</h3>
+      <h4>${product.title}</h4>
       <p>Category: ${product.category}</p>
-      <h2>Price: $ ${product.price}</h2>
+      <h4>Price: $ ${product.price}</h4>
       <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
       <button id="details-btn" class="btn btn-warning">Details</button></div>
       `;
